6867fde0c774f9f8a002a53b1868886391d77cc6
Eric Normand <ericwnormand@gmail.com>
2015-09-07 16:44:53 -0500

    Add first Om render.

--- src/lab_notebook/core.cljs
+++ src/lab_notebook/core.cljs
@@ -1,3 +1,15 @@
-(ns lab-notebook.core)
+(ns lab-notebook.core
+  (:require [om.core :as om]
+            [om.dom :as dom]))
 
 (defonce appstate (atom {}))
+
+(defn app-container [cursor component]
+  (reify
+    om/IRender
+    (render [this]
+      (dom/div nil "Hello!"))))
+
+(om/root app-container appstate
+  {:target (. js/document (getElementById "app"))})
+
